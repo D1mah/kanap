ef73fd55c35039e421ac03d2cc28a0465329e4a7
hamidBehlouli <behlouli.hamid@gmail.com>
2023-09-16 15:43:50 +0200

    comment rewrote

--- front/js/product.js
+++ front/js/product.js
@@ -1,13 +1,13 @@
 import { strNoAccent } from "./format.js";
 
-// Fetching system from API to get products.
-// Récupération des produits depuis l'API
+// ------------ Fetching system from API to get products.
+// ------------ Récupération des produits depuis l'API
 const reponse= await fetch('http://localhost:3000/api/products/');
 const products= await reponse.json();
 
 
-// Fetching  system to get item from name in URL
-// Récupération du produit par recherche du nom dans l'URL
+// ------------ Fetching  system to get item from name in URL
+// ------------ Récupération du produit par recherche du nom dans l'URL
 
 function findByName(){
     const urlObj= new URL(window.location.href);
@@ -22,15 +22,16 @@ function findByName(){
 
   const item= findByName();
 
-// Specific product generating function
+// ------------ fonction générant la page produit en fonction de l'item
+// ------------ Specific product generating function
   function generateProductPage(item){
     
-    // product element in product.html statement.
+    // Product element in product.html statement.
     // Récupération de l'élément du DOM qui accueillera le produit
     const sectionItem= document.querySelector(".item");
     
-    // Remplissage des balises
-    //Création balise img  
+    // Tags creation and filling 
+    // Création et remplisage des balises 
     const imgSection= document.querySelector(".item__img");
     const imgItem= document.createElement("img");
     imgItem.src=item.imageUrl;
@@ -43,6 +44,7 @@ function findByName(){
     const descriptionItem= document.querySelector("#description");
     descriptionItem.innerText=item.description;
     
+    // Color form options
     // Création des options du formulaire de choix de couleur
     const colorsItem= document.querySelector("#colors");
     for (let i=0; i<item.colors.length;i++){ 
